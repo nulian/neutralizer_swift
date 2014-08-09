7f05b66b939d6c04a1acff3262e123d7b36bbaa8
Peter Arentsen <peter.arentsen@gmail.com>
2014-08-10 00:46:20 +0200

    Initial Commit

---
+++ Neutralizer/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Neutralizer
+//
+//  Created by Peter Arentsen on 10-08-14.
+//  Copyright (c) 2014 Peter Arentsen. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
